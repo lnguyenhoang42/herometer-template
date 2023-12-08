11c7528336c263bbaab2e9c715249d161caf2a94
Inan <lnguyenhoang42@gmail.com>
2023-12-08 18:30:43 +0700

    Just testing

---
+++ src/main/java/net/inancoldflower/herometer/HeroMeterClient.java
@@ -0,0 +1,10 @@
+package net.inancoldflower.herometer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HeroMeterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
